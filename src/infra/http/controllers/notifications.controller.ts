3ca5294ca7adc06c83e4c2f95b17fdb92e22dcee
Ryan Vieira Souza <ryanvsouza99@gmail.com>
2022-12-14 10:56:32 -0300

    feat: create a conn with prisma to notifications repository

---
+++ src/infra/http/controllers/notifications.controller.ts
@@ -0,0 +1,21 @@
+import { PrismaService } from './../../database/prisma/prisma.service';
+import { CreateNotificationBody } from '../dtos/create-notification-body';
+import { Controller, Get, Body, Post } from '@nestjs/common';
+import { SendNotification } from 'src/application/use-cases/send-notification';
+
+@Controller('notifications')
+export class NotificationsController {
+  constructor(private sendNotification: SendNotification) {}
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { category, recipientId, content } = body;
+    const { notification } = await this.sendNotification.execute({
+      category,
+      recipientId,
+      content,
+    });
+
+    return { notification };
+  }
+}
